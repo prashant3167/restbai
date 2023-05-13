f55bf51c3bcff60641aa114f361caba2e56236f3
Prashant Gupta <prashantgupta@licious.com>
2023-05-13 02:49:28 +0200

    using host for correcttion

--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
 from flask import Flask, request
+from paste.translogger import TransLogger
+from waitress import serve
 
 app = Flask(__name__)
 
-@app.route('/upload/', methods=['POST'])
-def upload():
-    if 'image' not in request.files:
-        return 'No image uploaded.', 400
 
-    image = request.files['image']
+@app.route("/upload/", methods=["POST"])
+def upload():
+    if "image" not in request.files:
+        return "No image uploaded.", 400
+
+    image = request.files["image"]
     # You can save the image to a directory or perform any other processing here.
     # For simplicity, let's just return the name of the uploaded image.
-    return 'Image uploaded: ' + image.filename
+    return "Image uploaded: " + image.filename
 
 
-@app.route('/tets/', methods=['GET'])
+@app.route("/tets/", methods=["GET"])
 def test():
     # if 'image' not in request.files:
     #     return 'No image uploaded.', 400
@@ -21,8 +24,9 @@ def test():
     # image = request.files['image']
     # # You can save the image to a directory or perform any other processing here.
     # # For simplicity, let's just return the name of the uploaded image.
-    return 'Image uploaded:'
+    return {"hbc": "Image uploaded:"}
 
 
-if __name__ == '__main__':
-    app.run(port=8000)
+if __name__ == "__main__":
+    # app.run(port=8000)
+    serve(TransLogger(app), host="0.0.0.0", port=8000, threads=10)
