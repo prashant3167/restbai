2ad5515b4b024a503203a5230f5057ee6da53df1
Prashant Gupta <prashantgupta@licious.com>
2023-05-13 17:28:02 +0200

    last code

--- src/main.py
+++ src/main.py
@@ -1,24 +1,32 @@
 from flask import Flask, request
 from paste.translogger import TransLogger
 from waitress import serve
+# from flask_cors import CORS
+
+import os
 
 app = Flask(__name__)
+# CORS(app)
 
 
-@app.route("/upload/", methods=["POST"])
+@app.route("/upload", methods=["POST"])
 def upload():
-    if "image" not in request.files:
+    print(request.form.keys())
+    
+    if "files" not in request.files:
         return "No image uploaded.", 400
-
-    image = request.files["image"]
-    image.save('/app/static/test.jpeg')
-    # for file in request.files.getlist("upload"):
-    #     print(file)
-    #     filename = file.filename
-    #     destination = "/".join(['/app/static'])
-    #     print("Accept incoming file:", filename)
-    #     print(destination)
-    #     file.save('/app/static/test.jpeg')
+    # print(request.files["files"])
+    # # image = request.files["files"]
+    pid = request.form['name']
+    print(type(request.form))
+    os.makedirs('app/static/'+pid,exist_ok=True)
+    # # image.save('/app/static/test.jpeg')
+    data = request.form.to_dict() 
+    print(data)
+    for file in request.files.getlist("files"):
+        print(file)
+        filename = file.filename
+        file.save('app/static/'+pid+'/'+filename)
 
     # image.save('/app/'+image.filename)
     # You can save the image to a directory or perform any other processing here.
@@ -39,5 +47,5 @@ def test():
 
 
 if __name__ == "__main__":
-    # app.run(port=8000)
+    # app.run(port=8000,debug=True)
     serve(TransLogger(app), host="0.0.0.0", port=8000, threads=10)
