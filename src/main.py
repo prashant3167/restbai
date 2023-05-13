637434cba41ae8e30afc3112a9906967246f1935
Prashant Gupta <prashantgupta@licious.com>
2023-05-13 02:17:20 +0200

    test

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload/', methods=['POST'])
 def upload():
     if 'image' not in request.files:
         return 'No image uploaded.', 400
@@ -13,5 +13,16 @@ def upload():
     return 'Image uploaded: ' + image.filename
 
 
+@app.route('/tets/', methods=['GET'])
+def test():
+    # if 'image' not in request.files:
+    #     return 'No image uploaded.', 400
+
+    # image = request.files['image']
+    # # You can save the image to a directory or perform any other processing here.
+    # # For simplicity, let's just return the name of the uploaded image.
+    return 'Image uploaded:'
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000)
