28f22078497e96838756f44c47a1e96d03e1366e
Prashant Gupta <prashantgupta@licious.com>
2023-05-13 23:40:32 +0200

    Secomd page test

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,11 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Listing  from './Listing';
+import Slideshow from './Fade';
+import CitiesSlider from './script';
 import reportWebVitals from './reportWebVitals';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import { Routes ,Route } from 'react-router-dom';
+
+
+const slides = [
+  {
+    city: '',
+    country: 'Potico',
+    img: 'https://raw.githubusercontent.com/prashant3167/fastrestbai/main/static/restbai_test/323570__000.jpg',
+  },
+  {
+    city: 'Interior',
+    img: 'https://raw.githubusercontent.com/prashant3167/fastrestbai/main/static/restbai_test/323570__003.jpg',
+  },
+  {
+    city: 'Kitchen',
+    img: 'https://raw.githubusercontent.com/prashant3167/fastrestbai/main/static/restbai_test/323570__009.jpg',
+  }
+];
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Router> */}
+    <Router>
+    <Routes>
+    <Route path='/info/' element={<CitiesSlider slides={slides} />} />
+    <Route path='/' element={<App/>} />
+    </Routes>
+    </Router>
+
+      {/* <Switch>
+      <Route path="/info/">
+          <h1>test</h1>
+        </Route>
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
+    </Router> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
